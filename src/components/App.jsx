5cee0f62059034b5ef207d785a53b15690f30c4c
Frost15777 <di.yu.mk@gmail.com>
2024-01-05 20:31:33 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,14 @@ import { AppContainer } from './App.styled';
 
 class App extends Component {
 
-state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
-    };
-
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
 
   handleButtonClick = (type) => {
     this.setState((prevState) => ({
-      ...prevState,
       [type]: prevState[type] + 1
     }));
   };
@@ -36,35 +34,31 @@ state = {
     const { good, neutral, bad } = this.state;
     const totalFeedback = this.countTotalFeedback();
     const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-    let feedbackContent;
 
-if (totalFeedback === 0) {
-  feedbackContent = <Notification message="There is no feedback" />;
-} else {
-  feedbackContent = (
-    <Statistics
-      good={good}
-      neutral={neutral}
-      bad={bad}
-      total={totalFeedback}
-      positivePercentage={positiveFeedbackPercentage}
-    />
-  );
-}
+    return (
+      <AppContainer>
+        <Section title="Review Widget">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleButtonClick}
+          />
+        </Section>
 
-return (
-  <AppContainer>
-    <Section title="Review Widget">
-      <FeedbackOptions
-        options={['good', 'neutral', 'bad']}
-        onLeaveFeedback={this.handleButtonClick}
-      />
-    </Section>
-
-    <Section title="Statistics">{feedbackContent}</Section>
-  </AppContainer>
-);
-
+        <Section title="Statistics">
+          {totalFeedback === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positiveFeedbackPercentage}
+            />
+          )}
+        </Section>
+      </AppContainer>
+    );
   }
 }
 
